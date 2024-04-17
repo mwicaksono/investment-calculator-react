f43f9d1b130f1383a88accb35d9a9b2902422e8f
Masan Abdi Wicaksono <mwicaksono99@gmail.com>
2024-04-17 23:01:39 +0700

    init header, user input, and result components

---
+++ src/components/Result.jsx
@@ -0,0 +1,15 @@
+const Result = () => {
+    return (
+        <table id="result">
+            <thead>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Invested Capital</th>
+            </thead>
+        </table>
+    )
+}
+
+export default Result;
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,9 @@
+const UserInput = () => {
+    return (
+        <div id="user-input">
+            <p>Lorem, ipsum.</p>
+        </div>
+    )
+}
+
+export default UserInput;
