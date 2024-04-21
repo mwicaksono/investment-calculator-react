07e996cd4050bb2c43dfb7728002eb7fdb94ea78
Masan Abdi Wicaksono <mwicaksono99@gmail.com>
2024-04-21 22:58:10 +0700

    parse to int on input

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Header from "./components/Header"
 import Result from "./components/Result"
 import UserInput from "./components/UserInput"
-import { calculateInvestmentResults } from "./util/investment";
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -16,7 +15,7 @@ function App() {
     setUserInput((prevState) => {
       return {
         ...prevState,
-        [type]: newValue
+        [type]: parseInt(newValue)
       }
     });
 
@@ -26,7 +25,7 @@ function App() {
     <main>
       <Header />
       <UserInput userInput={userInput} handleInput={handleInput} />
-      <Result investment={calculateInvestmentResults(userInput)} />
+      <Result investment={userInput} />
     </main>
   )
 }
